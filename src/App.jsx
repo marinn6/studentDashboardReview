c9e604cb6654551a7fe34207e416ede96e560383
marinn6 <nmarin@pursuit.org>
2024-04-29 15:28:39 -0400

    deleted files

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-const App = () => {
+export const App = () => {
   return (
     <div>App</div>
   )
 }
-
-
-export default App
